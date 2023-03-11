9933ccbd0a5e33e221327c789a1bdc8fab0d1ae9
Shuo Tian <tianshuo1996@outlook.com>
2023-03-10 18:18:12 -0600

    article design

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,17 @@ module.exports = {
   important: true,
   theme: {
     extend: {},
+    colors: {
+      earth: {
+        50: "#F6F2E7",
+        100: "#EBE3CC",
+        200: "#DACDB3",
+        300: "#928976",
+        400: "#B1A893",
+        500: "#675744",
+      },
+      theme: "#D9B46F",
+    },
   },
   plugins: [],
 };
