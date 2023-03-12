5b3f9c700939c4aed12ccc4ee8ab48fcd360704d
Shuo Tian <tianshuo1996@outlook.com>
2023-03-11 23:19:20 -0600

    add styling to markdown

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,17 +3,18 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   important: true,
   theme: {
-    extend: {},
-    colors: {
-      earth: {
-        50: "#F6F2E7",
-        100: "#EBE3CC",
-        200: "#DACDB3",
-        300: "#928976",
-        400: "#B1A893",
-        500: "#675744",
+    extend: {
+      colors: {
+        earth: {
+          50: "#F6F2E7",
+          100: "#EBE3CC",
+          200: "#DACDB3",
+          300: "#928976",
+          400: "#B1A893",
+          500: "#675744",
+        },
+        theme: "#D9B46F",
       },
-      theme: "#D9B46F",
     },
   },
   plugins: [],
